54108ba2c8301ac3b1c73dd57155ca9daeb309a7
Jason Li <jasonli@dswinc.com>
2019-05-10 16:36:36 -0400

    refactor unit tests to use gwt semantic

--- spec/upperFirst.spec.js
+++ spec/upperFirst.spec.js
@@ -1,8 +1,8 @@
 const upperFirst = require("../src/upperFirst.js");
 
-describe("upperFirst", function() {
-    describe("called with non-string types", function () {
-        it("should have no errors", function () {
+given("upperFirst method", function() {
+    when("it is called with non-string types", function () {
+        then("it should have no errors", function () {
             let errors = this.checkUndesiredTypes({
                 desiredTypes: ["string"],
                 fn: upperFirst,
@@ -13,8 +13,8 @@ describe("upperFirst", function() {
         });
     });
 
-    describe("called with string", function () {
-        it("should return a new string with first letter turned to uppercase", function () {
+    when("it is called with string", function () {
+        then("it should return a new string with first letter turned to uppercase", function () {
             expect(upperFirst("")).toBe("");
             expect(upperFirst("a")).toBe("A");
             expect(upperFirst("ab")).toBe("Ab");
