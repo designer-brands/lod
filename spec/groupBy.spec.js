e6c806ed6053bd83d7af80ba9254ddc36ae3abb6
Jason Li <jasonli@dswinc.com>
2019-05-13 11:24:07 -0400

    add unit tests for groupBy.js

---
+++ spec/groupBy.spec.js
@@ -0,0 +1,29 @@
+const groupBy = require("../src/groupBy.js");
+
+given("groupBy method", function () {
+    when("called with a non-array type as the first argument ", function () {
+        then("it should have no errors", function () {
+            let errors = this.checkUndesiredTypes({
+                desiredTypes: ["array"],
+                fn: groupBy,
+                expectedValue: []
+            });
+
+            expect(errors).toBe("");
+        });
+    });
+
+    when("called with an array and an iteratee", function () {
+        then("it should group array values by iteratee's returns", function () {
+            let result = groupBy(
+                [3, 21, 34, 5, 2, 4, 3, 1, 2, 1],
+                val => val % 2 ? "odd" : "even"
+            );
+
+            expect(result).toEqual({
+                "odd": [3, 21, 5, 3, 1, 1],
+                "even": [34, 2, 4, 2]
+            });
+        });
+    });
+});
