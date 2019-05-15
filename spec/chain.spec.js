cfacf3c03c28ea79870b02505bf9c24bceecbfe1
Jason Li <jasonli@dswinc.com>
2019-05-15 15:24:03 -0400

    add unit tests for chain.js

---
+++ spec/chain.spec.js
@@ -0,0 +1,155 @@
+const chain = require("../src/chain.js");
+
+given("chain method", function () {
+    when("called with a value", function () {
+        then("it should return an object with 14 library methods used for chaining", function () {
+            let methods1 = Object.values(chain({}));
+
+            expect(methods1.length).toBe(14);
+
+            methods1.forEach(method => {
+                expect(method).toEqual(jasmine.any(Function));
+            });
+
+            let methods2 = Object.values(chain([]));
+
+            expect(methods2.length).toBe(14);
+
+            methods2.forEach(method => {
+                expect(method).toEqual(jasmine.any(Function));
+            });
+        });
+
+        then("chaining using library methods should work", function () {
+            let result1 = chain({})
+                .get("foo")
+                .value();
+
+            expect(result1).toBe(void 0);
+
+            let result2 = chain({})
+                .getAsNumber("foo")
+                .value();
+
+            expect(result2).toBe(0);
+
+            let result3 = chain({})
+                .getAsArray("foo")
+                .value();
+
+            expect(result3).toEqual([]);
+
+            let result4 = chain({})
+                .getAsObject("foo")
+                .value();
+
+            expect(result4).toEqual({});
+
+            let result5 = chain({})
+                .getAsString("foo")
+                .value();
+
+            expect(result5).toEqual("");
+
+            let result6 = chain({a: 1})
+                .get("a")
+                .value();
+
+            expect(result6).toBe(1);
+
+            let result7 = chain({a: 1})
+                .getAsNumber("a")
+                .value(1);
+
+            expect(result7).toBe(1);
+
+            let result8 = chain({a: 1})
+                .getAsArray("a")
+                .value();
+
+            expect(result8).toEqual([]);
+
+            let result9 = chain({a: 1})
+                .getAsObject("a")
+                .value();
+
+            expect(result9).toEqual({});
+
+            let result10 = chain({a: 1})
+                .getAsString("a")
+                .value();
+
+            expect(result10).toEqual("");
+
+            let obj = {
+                a: {
+                    b: [1, 2],
+                    c: 3,
+                    d: 4
+                },
+                e: 5
+            };
+
+            let longChainingResult1 = chain(obj)
+                .getAsObject("a")
+                .pick("b", "c")
+                .defaultToObject()
+                .value();
+
+            expect(longChainingResult1).toEqual({
+                b: [1, 2],
+                c: 3
+            });
+
+            let arr = [{
+                val: 1
+            }, {
+                val: 2
+            }, {
+                val: 3
+            }];
+
+            let longChainingResult2 = chain(arr)
+                .groupBy(element => element.val % 2 ? "odd" : "even")
+                .getAsNumber("odd.0.val")
+                .value();
+
+            expect(longChainingResult2).toEqual(1);
+        });
+
+        then("chaining using custom functions provided by invoke() method in the middle should work", function () {
+            let obj = {
+                a: [{
+                    name: "foo",
+                    id: "001",
+                    val: -1
+                }, {
+                    name: "bar",
+                    id: "002",
+                    val: 3
+                }, {
+                    name: "baz",
+                    val: -1
+                }],
+                b: "dummy"
+            };
+
+            let result = chain(obj)
+                .getAsArray("a")
+                .uniqBy(obj => obj.val)
+                .invoke(
+                    (tempArr, threshold) => tempArr.filter(element => element.val > threshold),
+                    0
+                )
+                .invoke(tempArr => tempArr.sort((a, b) => a.val > b.val))
+                .getAsObject("0")
+                .pick("val", "name")
+                .value();
+
+            expect(result).toEqual({
+                name: "bar",
+                val: 3
+            });
+        });
+    });
+});
