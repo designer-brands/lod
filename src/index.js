4f9cc503f8a37342dc8881f2f8707a29deb71378
Jason Li <jasonli@dswinc.com>
2019-05-15 10:51:25 -0400

    update index.js and unit tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const chain = require("./chain.js");
 const debounce = require("./debounce.js");
+const defaultToArray = require("./defaultToArray.js");
 const get = require("./get.js");
 const groupBy = require("./groupBy.js");
 const isNonEmptyObject = require("./isNonEmptyObject.js");
 const isNullOrUndefined = require("./isNullOrUndefined.js");
+const isObject = require("./isObject.js");
 const pick = require("./pick.js");
 const range = require("./range.js");
 const toCamelCase = require("./toCamelCase.js");
@@ -14,10 +16,12 @@ const upperFirst = require("./upperFirst.js");
 module.exports = {
     chain,
     debounce,
+    defaultToArray,
     get,
     groupBy,
     isNonEmptyObject,
     isNullOrUndefined,
+    isObject,
     pick,
     range,
     toCamelCase,
