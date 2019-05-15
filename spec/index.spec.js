d03a9be4bf1621483763a79faa842342de033423
Jason Li <jasonli@dswinc.com>
2019-05-15 11:08:11 -0400

    update index.js and unit tests

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,8 +1,8 @@
 const index = require("../src/index.js");
 
 given("index file", function () {
-    then("it should have 14 methods exposed", function () {
-        expect(Object.keys(index).length).toBe(14);
+    then("it should have 16 methods exposed", function () {
+        expect(Object.keys(index).length).toBe(16);
 
         Object.values(val => {
             expect(val).toEqual(jasmine.any(Function));
