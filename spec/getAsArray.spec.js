3148c38b0405524838a8c394613581007961f0f7
Jason Li <jasonli@dswinc.com>
2019-05-15 11:34:20 -0400

    add getAsArray.js and unit tests

---
+++ spec/getAsArray.spec.js
@@ -0,0 +1,45 @@
+const getAsArray = require("../src/getAsArray.js");
+
+given("getAsArray method", function () {
+    when("called with a value and a string path", function () {
+        let obj, arr;
+
+        beforeEach(function () {
+            obj = {
+                foo: {
+                    a: [1, 2],
+                    b: 3
+                },
+                bar: 4
+            };
+
+            arr = [{
+                foo: {
+                    a: [1, 2],
+                    b: 3
+                },
+                bar: 4
+            }];
+        });
+        then("it should return the value of the given path if it is an array", function () {
+            expect(getAsArray(obj, "foo.a")).toEqual([1, 2]);
+            expect(getAsArray(arr, "0.foo.a")).toEqual([1, 2]);
+        });
+
+        then("it should return an empty array if the value of the given path is not an array", function () {
+            expect(getAsArray(obj, "foo.b")).toEqual([]);
+            expect(getAsArray(obj, "bar")).toEqual([]);
+            expect(getAsArray(arr, "0.foo.b")).toEqual([]);
+            expect(getAsArray(arr, "0.bar")).toEqual([]);
+        });
+
+        then("it should return an empty array if the value of the given path does not exist", function () {
+            expect(getAsArray(obj, "foo.c")).toEqual([]);
+            expect(getAsArray(obj, "foo.c.d")).toEqual([]);
+            expect(getAsArray(obj, "baz")).toEqual([]);
+            expect(getAsArray(arr, "0.foo.c")).toEqual([]);
+            expect(getAsArray(arr, "0.foo.c.d")).toEqual([]);
+            expect(getAsArray(arr, "0.baz")).toEqual([]);
+        });
+    });
+});
---
+++ src/getAsArray.js
@@ -0,0 +1,6 @@
+const get = require("./get.js");
+const defaultToArray = require("./defaultToArray.js");
+
+module.exports = function getAsArray (obj, path) {
+    return defaultToArray(get(obj, path));
+}
