9af816ed3a43f5a9c64bd4ba539011cdf16e0a61
Jason Li <jasonli@dswinc.com>
2019-05-15 13:56:29 -0400

    add defaultToNumber.js and unit tests

---
+++ spec/defaultToNumber.spec.js
@@ -0,0 +1,22 @@
+const defaultToNumber = require("../src/defaultToNumber.js");
+
+given("defaultToNumber method", function () {
+    when("called with a non-number type", function () {
+        then("it should return an empty number as expected", function () {
+            let errors = this.checkUndesiredTypes({
+                desiredTypes: ["number"],
+                fn: defaultToNumber,
+                expectedValue: 0
+            });
+
+            expect(errors).toBe("");
+        });
+    });
+
+    when("called with a number", function () {
+        then("it should return the number itself", function () {
+            expect(defaultToNumber(0)).toBe(0);
+            expect(defaultToNumber(1)).toBe(1);
+        });
+    });
+});
