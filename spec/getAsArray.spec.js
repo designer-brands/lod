bcd751620ed0e763e79e4d18dc11013391c1db2a
Jason Li <jasonli@dswinc.com>
2019-05-15 11:57:21 -0400

    add unit tests for getAsArray.js to consider cases where the first arg is a primitive type

--- spec/getAsArray.spec.js
+++ spec/getAsArray.spec.js
@@ -21,6 +21,7 @@ given("getAsArray method", function () {
                 bar: 4
             }];
         });
+
         then("it should return the value of the given path if it is an array", function () {
             expect(getAsArray(obj, "foo.a")).toEqual([1, 2]);
             expect(getAsArray(arr, "0.foo.a")).toEqual([1, 2]);
@@ -31,6 +32,9 @@ given("getAsArray method", function () {
             expect(getAsArray(obj, "bar")).toEqual([]);
             expect(getAsArray(arr, "0.foo.b")).toEqual([]);
             expect(getAsArray(arr, "0.bar")).toEqual([]);
+            expect(getAsArray("abc", "length")).toEqual([]);
+            expect(getAsArray(1, "constructor")).toEqual([]);
+            expect(getAsArray(false, "constructor")).toEqual([]);
         });
 
         then("it should return an empty array if the value of the given path does not exist", function () {
@@ -40,6 +44,11 @@ given("getAsArray method", function () {
             expect(getAsArray(arr, "0.foo.c")).toEqual([]);
             expect(getAsArray(arr, "0.foo.c.d")).toEqual([]);
             expect(getAsArray(arr, "0.baz")).toEqual([]);
+            expect(getAsArray("abc", "foo")).toEqual([]);
+            expect(getAsArray(1, "foo")).toEqual([]);
+            expect(getAsArray(false, "foo")).toEqual([]);
+            expect(getAsArray(void 0, "foo")).toEqual([]);
+            expect(getAsArray(null, "foo")).toEqual([]);
         });
     });
 });
