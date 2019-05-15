60e4be1d99508cc031238a26ad5895aa43fe362e
Jason Li <jasonli@dswinc.com>
2019-05-15 10:59:02 -0400

    add defaultToObject.js and unit tests

---
+++ spec/defaultToObject.spec.js
@@ -0,0 +1,25 @@
+const defaultToObject = require("../src/defaultToObject.js");
+
+given("defaultToObject method", function () {
+    when("called with a non-object type", function () {
+        then("it should return an empty array as expected", function () {
+            let errors = this.checkUndesiredTypes({
+                desiredTypes: ["object"],
+                fn: defaultToObject,
+                expectedValue: {}
+            });
+
+            expect(errors).toBe("");
+        });
+    });
+
+    when("called with an object", function () {
+        then("it should return the object itself", function () {
+            let obj1 = {};
+            expect(defaultToObject(obj1)).toBe(obj1);
+
+            let obj2 = {a: 1};
+            expect(defaultToObject(obj2)).toBe(obj2);
+        });
+    });
+});
---
+++ src/defaultToObject.js
@@ -0,0 +1,5 @@
+const isObject = require("./isObject.js");
+
+module.exports = function defaultToObject (value) {
+    return isObject(value) ? value : {};
+};
